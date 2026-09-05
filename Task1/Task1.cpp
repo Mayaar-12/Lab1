0a6b5ba44aa21549ada2bef0c13ae8adc3be0aac
user <user@DESKTOP-8AKO433>
2026-09-05 19:05:13 +0300

    Add project files.

---
+++ Task1/Task1.cpp
@@ -0,0 +1,15 @@
+// Task1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include<iostream>
+using namespace std;
+int main() {
+double value1,value2,result;
+cout<<"Enter first value : ";
+cin>>value1;
+cout<<"Enter second value : ";
+cin>>value2;
+result = value1*value2;
+cout<<"Product is : "<<result;
+
+return 0;
+}
